d53b5c4b3ed9375e2be8d160feb5b5f73707ccdd
MartinGG2206 <mgil_559@unihumboldt.edu.co>
2025-03-04 12:29:30 -0500

    Delete ejercicio5.js

--- ejercicio5.js
+++
@@ -1,7 +0,0 @@
-let numero1 = prompt("Introduce un número");
-let numero2 = prompt("Introduce otro número");
-let numero3 = prompt("Introduce otro número");
-
-let numeroMayor = Math.max(numero1, numero2, numero3);
-
-document.getElementById('mensaje').innerHTML = `El numero mayor entre los ingresados es ${numeroMayor}`;
